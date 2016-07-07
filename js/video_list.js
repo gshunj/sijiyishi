8a9ff3a8327d57ddead073c112141d017c6615bf
jiangguoshun <491488041@qq.com>
2016-07-08 00:32:34 +0800

    实现视频列表--增加分页栏

--- js/video_list.js
+++ js/video_list.js
@@ -2,7 +2,6 @@
  * Created by jiangguoshun on 16/7/7.
  */
 $(function () {
-
     //主标签切换事件
     $('.content-wrapper .category-type-tags-wrapper .type-tags .type-tag .tag').click(function () {
         var _this = $(this);
@@ -13,11 +12,40 @@ $(function () {
         } else if (_this.hasClass('put-away-btn')) {
             _this_type_tag.find('.more').hide();
             _this_type_tag.find('.more-btn').show();
-        } else if (!_this.hasClass('btn-success')) {
+        } else if (!_this.hasClass('cur-active')) {
             _this_type_tag.find('.tag').each(function (inx, val) {
-                $(val).removeClass('btn-success');
+                $(val).removeClass('cur-active');
             });
-            _this.addClass('btn-success');
+            _this.addClass('cur-active');
         }
     });
+
+    //排序点击事件
+    $('.cur-videos-wrapper .cur-videos-header .sort-wrapper .sort').click(function () {
+        var _this = $(this);
+        var _this_i = _this.find('i');
+        if (_this_i.hasClass('undertone')) {
+            _this.closest('.sort-wrapper').find('.sort').each(function (inx, val) {
+                $(val).find('i').removeClass('thick').addClass('undertone');
+            });
+            _this_i.removeClass('undertone').addClass('thick');
+        } else {
+            _this_i.removeClass('thick').addClass('undertone');
+        }
+    });
+
+    //来源点击事件
+    $('.cur-videos-wrapper .cur-videos-header .from-type-wrapper .from-type').click(function () {
+        var _this = $(this);
+        var _this_i = _this.find('i');
+        if (_this_i.hasClass('undertone')) {
+            _this.closest('.from-type-wrapper').find('.from-type').each(function (inx, val) {
+                $(val).find('i').removeClass('thick').addClass('undertone');
+            });
+            _this_i.removeClass('undertone').addClass('thick');
+        } else {
+            _this_i.removeClass('thick').addClass('undertone');
+        }
+    });
+
 });
